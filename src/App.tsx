4085b00042394ecce6f28625f08437ec16069757
shun-harutaro <shun_harutaro@protonmail.com>
2022-07-01 14:54:19 +0900

    rewited all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Game from './components/game';
+import ButtonAppBar from './components/ButtonAppBar';
+import { GlobalStyles } from '@mui/material';
+import { css } from '@emotion/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <GlobalStyles styles={{ body: { margin: 0, padding: 0 } }} />
+        <ButtonAppBar />
+        <Game />
+      </div>
     </div>
   );
 }
--- src/components/game.tsx
+++ src/components/game.tsx
@@ -2,61 +2,8 @@ import React, { useState } from 'react';
 import Start from './start'
 import Funcs from './funcs'
 import Result from './result'
-/*
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      window: 'start',
-      data: {
-        typeCount: 0,
-        missCount: 0,
-        typeTime: 0,
-        //cpm: 0,
-        accuracy: 0,
-      }
-    }
-  }
 
-  startGame = () => {
-    this.setState({
-      window: 'play'
-    })
-  }
-
-  resetGame = () => {
-    this.setState({
-      window: 'start'
-    })
-  }
-
-  getData = (resultData) => {
-    this.setState({
-      window: 'result',
-      data: {
-        typeCount: resultData.typeCount,
-        missCount: resultData.missCount,
-        typeTime: resultData.typeTime,
-        //cpm: resultData.cpm,
-        accuracy: resultData.accuracy,
-      }
-    })
-  }
-
-  render() {
-    const current_window = this.state.window;
-    if (current_window === 'start') {
-      return (<Start startGame={this.startGame} />)
-    } else if (current_window === 'play') {
-      return (<Type setResult={this.getData}/>)
-    } else if (current_window === 'result') {
-      const resultData = this.state.data;
-      return (<Result data={resultData} startGame={this.resetGame}/>)
-    }
-  }
-};*/
-
-const Game = () => {
+const Game:React.FC = () => {
   const [window, setWindow] = useState('start');
   const [datas, setData] = useState(
     {
